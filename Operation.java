6b0b7c2e27ffd0de9aff64dfae4d088e1fe0ba28
ayushnenawati <ayushnenawati@gmail.com>
2016-09-22 19:23:07 +0530

    Update Operation.java

--- Operation.java
+++ Operation.java
@@ -1,6 +1,6 @@
 /* Program for Operation */
 class Main{
-public static void main(String args[]){
-  System.out.println("sum");
-}
+  public static void main(String args[]){
+    System.out.println("sum");
+  }
 }
