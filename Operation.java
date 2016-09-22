59853bcf8bb04c1e2ec7379e0d0b646d169884fb
Ayush Nenawati <ayush.n@peopleinteractive.in>
2016-09-22 19:19:17 +0530

    addingmultiply

--- Operation.java
+++ Operation.java
@@ -1,6 +1,15 @@
 /* Program for Operation */
+class Multiply{
+
+	int multiply(int x,int y){
+		return x*y;
+	}
+
+}
 class Main{
 	public static void main(String args[]){
-  		System.out.println("sum");
+		Multiply ob = new Multiply();
+  		System.out.println("multiplication");
+  		System.out.println(ob.multiply(1,2));
 	}
 }
